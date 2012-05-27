1eb80dd5e12d2d8609d327f706c706da8cd60117
Michel Salib <michelsalib@hotmail.com>
2012-05-27 21:25:48 +0200

    Update to reflect last symfony (master) changes - migrate tests to composer

--- Form/Type/CronType.php
+++ Form/Type/CronType.php
@@ -2,24 +2,12 @@
 
 namespace BCC\CronManagerBundle\Form\Type;
 
-use \Symfony\Component\Form\FormBuilder;
+use \Symfony\Component\Form\FormBuilderInterface;
 use \Symfony\Component\Form\AbstractType;
 
 class CronType extends AbstractType
 {
-    /**
-     * Builds the form.
-     *
-     * This method gets called for each type in the hierarchy starting from the
-     * top most type.
-     * Type extensions can further modify the form.
-     *
-     * @see FormTypeExtensionInterface::buildForm()
-     *
-     * @param \Symfony\Component\Form\FormBuilder $builder The form builder
-     * @param array         $options The options
-     */
-    public function buildForm(FormBuilder $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('minute');
         $builder->add('hour');
@@ -39,14 +27,8 @@ class CronType extends AbstractType
         ));
     }
 
-    /**
-     * Returns the default options for this type.
-     *
-     * @param array $options
-     *
-     * @return array The default options
-     */
-    public function getDefaultOptions() {
+    public function getDefaultOptions()
+    {
         return array(
             'data_class' => 'BCC\CronManagerBundle\Manager\Cron',
         );
